69a5847d159153c37bba150bec99fd1da502c0fc
Michael Oyer <code@michaeloyer.com>
2023-12-02 13:18:46 -0500

    F# 2023-02 Complete

---
+++ F#/2023/02/code.fsx
@@ -0,0 +1,87 @@
+#r "nuget:FParsec"
+
+type PuzzleInput = { FullText: string; Lines: string list }
+
+module AdventOfCode =
+    open FParsec
+
+    type Cubes = {
+        Red: int
+        Green: int
+        Blue: int
+    } with static member Default = { Red = 0; Blue = 0; Green = 0 }
+
+    type Game = {
+        Id: int
+        Rounds: Cubes list
+    }
+
+    let readGame line =
+        let cubeCount color = pint32 .>> spaces .>> skipString color
+        let round =
+            choice [
+                attempt(cubeCount "red" |>> fun count -> { Cubes.Default with Red = count })
+                attempt(cubeCount "green" |>> fun count -> { Cubes.Default with Green = count })
+                attempt(cubeCount "blue" |>> fun count -> { Cubes.Default with Blue = count })
+            ]
+
+        let rounds =
+            sepBy1 round (skipChar ',' .>> spaces)
+            |>> List.reduce (fun roundA roundB -> {
+                Red = roundA.Red + roundB.Red
+                Blue = roundA.Blue + roundB.Blue
+                Green = roundA.Green + roundB.Green
+            })
+
+        let game =
+            pstring "Game " >>. pint32 .>> skipString ": " .>>. sepBy1 rounds (skipChar ';' .>> spaces)
+            |>> fun (id, rounds) -> { Id = id; Rounds = rounds }
+
+        run game line |> function
+        | Success (game, _, _) -> game
+        | Failure (err, _, _) -> failwith err
+    let part1 (puzzle:PuzzleInput) = List.sum [
+        for line in puzzle.Lines do
+            let game = readGame line
+            let allRoundsPossible =
+                game.Rounds
+                |> List.forall (fun round ->
+                    round.Red <= 12 &&
+                    round.Green <= 13 &&
+                    round.Blue <= 14)
+
+            if allRoundsPossible then
+                yield game.Id
+    ]
+
+    let part2 (puzzle:PuzzleInput) = List.sum [
+        for line in puzzle.Lines do
+            let game = readGame line
+
+            let minimumCubes =
+                game.Rounds
+                |> List.reduce (fun round1 round2 ->
+                    { Red = max round1.Red round2.Red
+                      Green = max round1.Green round2.Green
+                      Blue = max round1.Blue round2.Blue }
+                )
+            yield minimumCubes.Red * minimumCubes.Green * minimumCubes.Blue
+    ]
+
+module Input =
+    open System.IO
+    let private readData name =
+        let lines = File.ReadAllLines(Path.Combine(__SOURCE_DIRECTORY__, $"{name}.txt"))
+        { Lines = List.ofArray lines
+          FullText = String.concat "\n" lines }
+
+    let rec example = readData (nameof example)
+    let rec puzzle = readData (nameof puzzle)
+
+module Output =
+    type PuzzleInputOutput = { LineCount: int }
+    fsi.AddPrintTransformer(fun (input:PuzzleInput) -> { LineCount = input.Lines.Length })
+    let rec ``Example Part 1`` = let answer = AdventOfCode.part1 Input.example in printfn $"{nameof ``Example Part 1``}: {answer}"; answer
+    let rec ``Puzzle Part 1`` = let answer = AdventOfCode.part1 Input.puzzle in printfn $"{nameof ``Puzzle Part 1``}: {answer}"; answer
+    let rec ``Example Part 2`` = let answer = AdventOfCode.part2 Input.example in printfn $"{nameof ``Example Part 2``}: {answer}"; answer
+    let rec ``Puzzle Part 2`` = let answer = AdventOfCode.part2 Input.puzzle in printfn $"{nameof ``Puzzle Part 2``}: {answer}"; answer
